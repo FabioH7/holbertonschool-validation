b8fb5de016cc57a4d239a6620412da09961d1d46
FabioH7 <fhysollari4@gmail.com>
2023-05-02 14:46:18 +0200

    adding

--- module1_task4/setup.sh
+++ module1_task4/setup.sh
@@ -1,4 +1,3 @@
-apt-get update && apt-get install -y hugo make
-hugo
-printf "recipe for target 'build' failed"
-exit 255
+#!/bin/bash
+apt-get update && apt-get install -y git make wget build-essential && wget https://github.com/gohugoio/hugo/releases/download/v0.109.0/hugo_extended_0.109.0_linux-amd64.deb && dpkg -i hugo_extended_0.109.0_linux-amd64.deb 
+make build
