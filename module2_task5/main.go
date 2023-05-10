f994b4fd7abf837c89a6c354f9469db7bb8933ae
FabioH7 <fhysollari4@gmail.com>
2023-05-10 13:33:48 +0200

    fix

--- module2_task5/main.go
+++ module2_task5/main.go
@@ -26,7 +26,7 @@ func setupRouter() *mux.Router {
 	r := mux.NewRouter()
 
 	// When an HTTP GET request is received on the path /health, delegates to the function "HealthCheckHandler()"
-	r.HandleFunc("/healthcheck", HealthCheckHandler).Methods("GET")
+	r.HandleFunc("/health", HealthCheckHandler).Methods("GET")
 	// when an HTTP GET request is received on the path /hello
 	r.HandleFunc("/hello", HelloHandler).Methods("GET")
 
