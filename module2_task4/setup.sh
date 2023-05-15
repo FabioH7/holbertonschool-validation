129b971b1069813ea05aee0f395717c3dbc0c219
FabioH7 <fhysollari4@gmail.com>
2023-05-15 14:36:46 +0200

    adding all

--- module2_task4/setup.sh
+++ module2_task4/setup.sh
@@ -1,3 +1,4 @@
 #!/bin/bash
 apt-get update && apt-get install -y git make wget build-essential && wget https://github.com/gohugoio/hugo/releases/download/v0.109.0/hugo_extended_0.109.0_linux-amd64.deb && dpkg -i hugo_extended_0.109.0_linux-amd64.deb 
+
 make build
